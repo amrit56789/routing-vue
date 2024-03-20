a6c911095c42f90fe83a7cfed87d2d80564583ef
amritsingh <amritsingh@innotechteam.in>
2024-03-20 19:18:48 +0530

    routing

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  font-size: 25px;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+  background-color: crimson;
+}
+</style>
---
+++ src/views/jobs.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="jobs">
+    <h1 class="jobsHead">JOBS</h1>
+    <div class="jobWrapper">
+    <div v-for="job in jobs" :key="job.id" class="jobHead">
+      <h2 >  {{job.title}}   {{job.details}} </h2>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "jobsView",
+  data() {
+    return {
+      jobs: [
+        { title: "Ux Designer", id: 1, details: "loream" },
+        { title: "Web Developer", id: 2, details: "loream" },
+        { title: "Node Developer", id: 3, details: "Korean" },
+      ],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.jobsHead{
+    margin-top: 40px;
+    font-size: 50px;
+    letter-spacing: 1px;
+}
+.jobWrapper{
+    margin-top: 50px;
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+}
+.jobHead{
+    font-size: 20px;
+    margin-top: 10px;
+    width: 35%;
+    background-color: #d4d6d9;
+    color: #3f4040;
+    border-radius: 5px;
+}
+</style>
